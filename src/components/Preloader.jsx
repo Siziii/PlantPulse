f05374d060cd55344a4dd10aa1eb92f2dccd4ccf
Siziii <sizi.dev@gmail.com>
2023-10-20 01:07:07 +0200

    flipped preloader exit animation

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -4,10 +4,10 @@ import logo from '../assets/logo.svg';
 const Preloader = () => {
   return (
     <motion.div
-      initial={{ clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)' }}
+      initial={{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)' }}
       animate={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }}
-      exit={{ clipPath: 'polygon(0 0, 100% 0, 100% 0, 0 0)' }}
-      transition={{ duration: 0.5, type: 'tween' }}
+      exit={{ clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)' }}
+      transition={{ duration: 0.3, type: 'tween' }}
       className="bg-dark-primary w-screen h-screen fixed z-50 flex justify-center items-center overflow-hidden"
     >
       <img src={logo} alt="logo" className="h-20 animate-pulse" />
